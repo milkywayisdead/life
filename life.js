43d96a0a048143dd39cf8ad445a50de75db82fd4
milkywayisdead <existentiary@gmail.com>
2022-08-22 10:58:06 +0300

    life реализ. в виде класса

--- life.js
+++ life.js
@@ -1,126 +1,125 @@
 document.addEventListener('DOMContentLoaded', e => {
-    let w = 50;
-    let h = 50;
-
-    let cells = {};
-    let stateMatrix = [];
-
-    for(let i = 0; i < h; i++){
-        let row = document.createElement('tr');
-        stateMatrix[i] = [];
-        for(let j = 0; j < w; j++){
-            const cell = document.createElement('td');
-            cell.classList.add('dead');
-            cell.setAttribute('data-xy', `${j}:${i}`);
-            cells[`${j}:${i}`] = cell;
-            cell.addEventListener('click', e => {
-                const c = e.target;
-                _killOrCreate(c);
-            });
-            row.append(cell);
-            stateMatrix[i][j] = 0;
-        }
-        document.getElementById('board').append(row);
-    }
+    const w = 20;
+    const h = 20;
+    const life = new Life(w, h, '#board');
 
     let gameOn = false;
     document.getElementById('start-btn').addEventListener('click', e => {
         gameOn = true;
         setInterval(_ => {
             if(gameOn){
-                life();
+                life.nextGeneration();
             }
         }, 80);
     });
     document.getElementById('stop-btn').addEventListener('click', e => {
         gameOn = false;
     });
+});
 
 
-    const life = _ => {
-        const p = processStateMatrix(stateMatrix);
-        stateMatrix = p[0];
-        const updates = p[1];
-        applyUpdates(updates, cells);
+class Life {
+    constructor(w, h, boardSelector){
+        const rows = [];
+        const statesMapping = {};
+        
+        for(let i = 0; i < h; i++){
+            let row = document.createElement('tr');
+            for(let j = 0; j < w; j++){
+                const cell = document.createElement('td');
+                cell.classList.add('dead');
+                const xyStr = `${j}:${i}`;
+                cell.addEventListener('click', e => {
+                    const c = e.target;
+                    const classList = c.classList;
+                    if(classList.contains('dead')){
+                        classList.replace('dead', 'live');
+                        statesMapping[xyStr].state = 1;
+                    } else {
+                        classList.replace('live', 'dead');
+                        statesMapping[xyStr].state = 0;
+                    }
+                });
+                row.append(cell);
+                statesMapping[xyStr] = new Cell(0, cell);
+            }
+            rows.push(row)
+        }
+
+        this.statesMapping = statesMapping;
+        const board = document.querySelector(boardSelector);
+        for(let row of rows){
+            board.append(row);
+        }
     }
 
-    const processStateMatrix = (smatrix) => {
+    // создать следующее поколение
+    nextGeneration(){
+        this._applyUpdates(this._getUpdates());
+    }
+
+    // получение списка клеток, состояние которых нужно изменить в след. поколении
+    _getUpdates(){
         const updates = [];
-        const updatedMatrix = [];
-        smatrix.forEach((row, y) => {
-            updatedMatrix[y] = [];
-            row.forEach((value, x) => {
-                updatedMatrix[y][x] = value;
-                const n = _getNeighborsStatesSum(x, y, smatrix);
-                const xyStr = `${x}:${y}`;
-                if(value === 1){
-                    if(n < 2 || n > 3){
-                        updatedMatrix[y][x] = 0;
-                        updates.push(xyStr);
-                    }
-                } else {
-                    if(n === 3){
-                        updatedMatrix[y][x] = 1;
-                        updates.push(xyStr);
-                    }
+        for(let s in this.statesMapping){
+            const cell = this.statesMapping[s]
+            const xy = s.split(':');
+            const x = Number(xy[0]);
+            const y = Number(xy[1]);
+
+            // перебор соседей клетки
+            const neighbors = [
+                {x: x-1, y: y-1}, {x: x, y: y-1}, {x: x+1, y: y-1},
+                {x: x-1, y: y}, {x: x+1, y: y},
+                {x: x-1, y: y+1}, {x: x, y: y+1}, {x: x+1, y: y+1},
+            ];
+            let statesSum = 0; // сумма состояний соседей
+            for(let n of neighbors){
+                const neighbor = this.statesMapping[`${n.x}:${n.y}`];
+                if(neighbor !== undefined){
+                    statesSum += neighbor.state;
+                } 
+            }
+            if(cell.state === 1){
+                if(statesSum < 2 || statesSum > 3){
+                    updates.push(s);
                 }
-            });
-        });
-
-        return [updatedMatrix, updates];
-    }
-
-    const applyUpdates = (updates, cells) => {
-        for(let u of updates){
-            const c = cells[u];
-            if(c.classList.contains('dead')){
-                _create(c);
             } else {
-                _kill(c);
+                if(statesSum === 3){
+                    updates.push(s);
+                }
             }                
         }
+
+        return updates;       
     }
 
-    const getCellCoords = (cell) => {
-        let xy = cell.getAttribute('data-xy');
-        xy = xy.split(':');
-        return {x: Number(xy[0]), y: Number(xy[1])};
-    }
-
-    const _getNeighborsStatesSum = (x, y, smatrix) => {
-        const neighbors = [
-            {x: x-1, y: y-1}, {x: x, y: y-1}, {x: x+1, y: y-1},
-            {x: x-1, y: y}, {x: x+1, y: y},
-            {x: x-1, y: y+1}, {x: x, y: y+1}, {x: x+1, y: y+1},
-        ];
-        let statesSum = 0;
-        for(let n of neighbors){
-            try {
-                smatrix[n.y][n.x];
-            } catch {
-                continue;
-            }
-            statesSum += smatrix[n.y][n.x];;
+    // применение обновлений к карте состояний
+    _applyUpdates(updates){
+        for(let c of updates){
+            this.statesMapping[c].changeState();
         }
-        return statesSum;
+    }
+}
+
+
+// клетка
+class Cell {
+    constructor(state, td){
+        this.state = state; // состояние, 0 - мертвая, 1 - живая
+        this.td = td; // соответствующий td-элемент 
     }
 
-    const _kill = (c) => {
-        c.classList.replace('live', 'dead');
-    }
-
-    const _create = (c) => {
-        c.classList.replace('dead', 'live');
-    }
-
-    const _killOrCreate = c => {
-        const {x, y} = getCellCoords(c);
-        if(c.classList.contains('dead')){
-            _create(c);
-            stateMatrix[y][x] = 1;
+    // смена состояния и изменение соотв. элемента
+    changeState(){
+        const s = this.state;
+        this.state = s === 0 ? 1 : 0;
+        const classList = this.td.classList;
+        if(classList.contains('dead')){
+            classList.replace('dead', 'live');
         } else {
-            _kill(c);
-            stateMatrix[y][x] = 0;
+            classList.replace('live', 'dead');
         }
     }
-});
+}
+
